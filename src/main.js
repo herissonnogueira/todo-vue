f4c20cd0ba6186430443528bcbf0f5649af17a6c
Herisson Nogueira <herisson.nogueira002@gmail.com>
2023-09-19 16:17:08 -0300

    Configurando Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+// Importando o css do bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app')
